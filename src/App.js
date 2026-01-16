b2054b3c9e47d916d140cd905fb6747cdb39f593
Your Name <your-email@example.com>
2026-01-16 15:33:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Login from './Pages/Login';
 import {useState} from 'react';
 import Footer from './Components/Footer';
+import Cart from './Cart.js';
 
 
-function App() {
+
+function App(fetchdata) {
  
   const [cart,setcart]=useState([]);
-  const Addtocart=(product)=>{
-    console.log(product,"hello")
+  const Addtocart=()=>{
+    console.log(fetchdata,"hello")
     const cartproduct=[
       {
-        id:product.id,
-        title:product.title,
-        image:product.image
+        id:fetchdata.id,
+        title:fetchdata.title,
+        image:fetchdata.image
       }
     ]
     setcart(prevState=>[...prevState,...cartproduct]);
@@ -25,16 +27,22 @@ function App() {
   console.log(cart)
 
 
+  const accessToken=localStorage.getItem('token')
+  var user=JSON.parse(localStorage.getItem("userdata"))
+
+
 
   return (
     <div className="App">
-     
-     <Header cartdata={cart}/>
+      {accessToken &&(
+     <Header cartdata={cart} Loginuser={user}/>
+      )}
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Maincomponent Addtocart={Addtocart}/>}/>
         <Route path='/details/:id' element={<CardDetails/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/cart' element={<Cart Cartdisplay={cart}/>}/>
       </Routes>
      </BrowserRouter>
      <Footer/>
